c85e6c22664e64e4fc451967f877a1428fde4992
root <liyinda0000@163.com>
2019-01-21 19:18:46 +0800

    init

---
+++ main.go
@@ -0,0 +1,138 @@
+/*
+Copyright 2018 The AmrToMp3 Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package main
+ 
+import (
+  "os"
+  "strings"
+  "time"
+  "./models"
+  "fmt"
+  "path"
+  "runtime"
+  "flag"
+  "log"
+)
+
+var (
+  filePath = flag.String("path", "./", "Audio Conversion Path")
+  logFileName = flag.String("log", "audio.log", "Log File Name")
+)
+ 
+func main() {
+  flag.Parse()
+  for {
+    arm2mp3_control()
+    time.Sleep(1e9)
+  }
+}
+ 
+//amr转mp3控制函数
+func arm2mp3_control() {
+  //请求转换的工作目录
+  WORKDIR := *filePath + "/work/"
+  //备份目录
+  BAKDIR := *filePath + "/bak/"
+  //产品目录
+  PRODUCTDIR := *filePath + "/audio/"
+
+  fmt.Println(PRODUCTDIR)
+
+  //获取cpu个数
+  cpuNum := runtime.NumCPU()
+  //读取目录下的文件信息
+  filelist := models.GetDirFiles(WORKDIR)
+  if filelist == nil {
+    return
+  }
+  fmt.Println("filelist =", filelist)
+
+  //获取当前日期
+  today := time.Now().Format("20060102")
+  //初始化判断是否存在日期目录，如果没有生成对应生产目录
+  todaydir := PRODUCTDIR + today
+  models.MkDir(todaydir)
+  //初始化判断是否存在日期目录，如果没有生成对应备份目录 
+  todaybakdir := BAKDIR + today 
+  models.MkDir(todaybakdir)
+
+  //将filelist的文件列表传入ch管道 
+  ch :=make(chan string)
+
+  //创建goroutine获取文件列表并将列表传递给管道另一端进行音频转换
+  go func(mine []os.FileInfo) {
+    for _, item := range mine {
+      ch <- item.Name()
+    }
+  }(filelist)
+
+  go func() {
+    for i :=0; i<= cpuNum; i++ {
+      file := <-ch
+
+      //获取文件的内容
+      wfilepath := WORKDIR + file
+      bfilepath := BAKDIR + today + "/" + file
+      pfilepath := PRODUCTDIR + today + "/" + file
+  
+      var rs bool
+      var fileExt string
+      //获取文件名后缀
+      fileExt = path.Ext(file) 
+      newfilepath := ""
+      audiofilepath := ""
+
+      //操作日志记录
+      logFile, logErr := os.OpenFile(*logFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+      if logErr != nil {
+        fmt.Println("Fail to find", *logFile, "AmrToMp3 start Failed")
+        os.Exit(1)
+      }
+      defer logFile.Close()
+      log.SetOutput(logFile)
+      log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+  
+      switch {
+        case fileExt == ".amr" :
+          //进行amr到mp3的转换
+          newfilepath = strings.Replace(wfilepath, ".amr", ".mp3", -1)
+          comeAndTo := [...]string{wfilepath, newfilepath}
+          rs = models.Arm2mp3(comeAndTo)
+          log.Printf("%v Amr format conversion success \n", newfilepath)
+        case fileExt == ".wav" :
+          //进行wav到mp3的转换
+          newfilepath = strings.Replace(wfilepath, ".wav", ".mp3", -1)
+          comeAndTo := [...]string{wfilepath, newfilepath}
+          rs = models.Arm2mp3(comeAndTo)
+          log.Printf("%v Wav format conversion success \n", newfilepath)
+        default:
+          //os.Remove(wfilepath)
+          os.Rename(wfilepath, bfilepath)
+          log.Printf("%v conversion fail and backup \n", bfilepath)
+      }
+  
+      if rs {
+        //转换成功之后，将原始文件移动到备份目录
+        os.Rename(wfilepath, bfilepath)
+        //生产mp3文件路径
+        audiofilepath = strings.Replace(pfilepath, ".amr", ".mp3", -1)
+        //转换成功之后，将生成文件移动到生产目录
+        os.Rename(newfilepath, audiofilepath)
+      }
+    }
+  }()
+
+}
+ 
---
+++ models/models.go
@@ -0,0 +1,113 @@
+//执行函数命令包
+//作者：bjlfp
+//邮箱：bjlfp@163.com
+//创建时间：2018-9-13
+ 
+package models
+ 
+import (
+    "os/exec"
+    "os"
+    "fmt"
+    "io/ioutil"
+)
+ 
+//执行命令函数: 不能感知命令的执行信息，只返回是否执行成功
+//commandName 命名名称，如cat，ls，git等
+//params 命令参数，如ls -l的-l，git log 的log等
+func execCommand(commandName string, params []string) bool {
+    cmd := exec.Command(commandName, params...)
+ 
+    err := cmd.Start()
+    if err != nil {
+        return false
+    }
+ 
+    err = cmd.Wait()
+    if err != nil {
+        return false
+    }
+ 
+    return true
+}
+
+
+//如果文件夹不存在，创建文件夹
+func MkDir(path string) {
+  exist, err := pathExists(path)
+  if err != nil {
+    fmt.Printf("get dir error![%v]\n", err)
+    return
+  }
+  if exist {
+    fmt.Printf("has dir![%v]\n", path)
+    //continue
+  } else {
+    err := os.Mkdir(path, os.ModePerm)
+    if err != nil {
+      fmt.Printf("mkdir failed![%v]\n", err)
+    } else {
+      fmt.Printf("mkdir success![%v]\n", path)
+    }
+  }
+}
+
+
+//判断文件夹是否存在
+func pathExists(path string) (bool, error) {
+  _, err := os.Stat(path)
+  if err == nil {
+      return true, nil
+  }
+  if os.IsNotExist(err) {
+      return false, nil
+  }
+  return false, err
+}
+
+
+//遍历目录下的文件
+//path 遍历的目录
+//返回文件记录的切片
+//cpunum等于系统核心数   
+func GetDirFiles(path string) []os.FileInfo {
+  //最大处理文件数为100
+  var filelist [100]os.FileInfo
+  var i int = 0
+
+  dir, err := ioutil.ReadDir(path)
+  if err != nil {
+    return nil
+  }
+
+  for _, file := range dir {
+    if file.IsDir() {
+      continue
+    }
+
+    if i >= 100 {
+      break
+    }
+
+    filelist[i] = file
+    i++
+  }
+
+  if i == 0 {
+    return nil
+  }
+
+  return filelist[0:i]
+}
+
+//进行amr到mp3音频格式的转换
+//farr 文件信息：0arm文件名；1mp3文件名
+//返回是否成功：true成功，false失败
+func Arm2mp3(farr [2]string) bool {
+  //执行命令ffmpeg -i 1.amr 1.mp3
+  params := [...]string{"-i", farr[0], farr[1]}
+  //fmt.Print(params[0:3])
+  rs := execCommand("ffmpeg", params[0:3])
+
+  return rs
+}
